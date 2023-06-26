fe2f7de484d255bd466ff426f2c547f5259d914e
Kan Yuenyong <sikkha@gmail.com>
2023-06-26 10:06:18 +0700

    revise shell script

--- src/call_summarize_web.sh
+++ src/call_summarize_web.sh
@@ -6,8 +6,9 @@ export PINECONE_INDEX_NAME="YOUR_PINECONE_INDEX"
 export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"
 
 # to be automated on process tweet later
+cd $HOME/PulsarWave/src
 python3 $HOME/PulsarWave/src/webcrawling.py https://www.washingtonpost.com/national-security/2023/06/24/us-intelligence-prigozhin-putin/ > web.txt
 
-python3 $HOME/PulsarWave/src/showcase_pinecone.py
+python3 $HOME/PulsarWave/src/showcase_pinecone.py > $HOME/PulsarWave/src/showcase_pinecone_output.txt
 
-python3 $HOME/PulsarWave/src/send_email2.py "Showcase Pinecone report" < $HOME/showcase_pinecone_output1.txt 
+python3 $HOME/PulsarWave/src/send_email2.py "Showcase Pinecone report" < $HOME/PulsarWave/src/showcase_pinecone_output.txt 

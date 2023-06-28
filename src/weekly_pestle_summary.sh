6fa5a5b41246784379fb8f53598e8068a113c3d4
Kan Yuenyong <sikkha@gmail.com>
2023-06-28 17:19:36 +0700

    misspelling directory name

--- src/weekly_pestle_summary.sh
+++ src/weekly_pestle_summary.sh
@@ -3,11 +3,11 @@
 #API Section
 export OPENAI_API_KEY="YOUR_OPENAI_API_KEY"
 
-
+cd $HOME/PulsarWave/src
 python3 $HOME/PulsarWave/src/final_process_tweet.py 
 python3 $HOME/PulsarWave/src/ai_weekly_pestle.py
 python3 $HOME/PulsarWave/src/send_email2.py "Weekly Metageopolitical PESTLE" < /tmp/superb.txt
 
-cp $HOME/revised_accu_process_tweet.txt $HOME/PulsarWave/trend-radar/docs/entry.txt
-cd $HOME/PulsarWave/docs
+cp $HOME/PulsarWave/src/revised_accu_process_tweet.txt $HOME/PulsarWave/trend-radar/docs/entry.txt
+cd $HOME/PulsarWave/trend-radar/docs
 $HOME/PulsarWave/trend-radar/docs/generate_trend_radar.sh
--- trend-radar/docs/generate_trend_radar.sh
+++ trend-radar/docs/generate_trend_radar.sh
@@ -4,16 +4,16 @@
 cp index.html "index.html,$(date +%Y-%m-%d_%H-%M-%S)_back"
 
 # Step 2: Generate the header
-python print_header.py
+python3 print_header.py
 
 # Step 3: Loop through the entries in entry.txt and call print_entry.py
 while IFS=',' read -r quadrant ring label link active moved
 do
-    python print_entry.py "$quadrant" "$ring" "$label" "$link" "$active" "$moved"
+    python3 print_entry.py "$quadrant" "$ring" "$label" "$link" "$active" "$moved"
 done < entry.txt
 
 # Step 4: Generate the footer
-python print_footer.py
+python3 print_footer.py
 
 # Step 5: rewrite the index.html
 cp new_index.html index.html
